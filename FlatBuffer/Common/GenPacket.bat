22e3925dbc2a58392478e4d4594562f5328c1679
ssmktr87 <ssmktr@gmail.com>
2025-09-14 01:51:25 +0900

    c# flatbuffer 생성 및 복사

--- FlatBuffer/Common/GenPacket.bat
+++ FlatBuffer/Common/GenPacket.bat
@@ -1,9 +1,18 @@
 .\flatc.exe -c protocol.fbs
+.\flatc --csharp protocol.fbs
 if errorlevel 1 pause
 
+REM =============== C++ Protocl ===============
 xcopy /y protocol_generated.h "../Server"
 xcopy /y protocol_generated.h "../ConsoleClient"
 xcopy /y protocol_generated.h "../MFCServer"
 xcopy /y protocol_generated.h "../MFCClient"
 
 del protocol_generated.h
+
+
+REM =============== C# Protocl ===============
+ren fbs Protocol
+xcopy /y /e /i Protocol "../CSharpClientClient/Protocol"
+
+rmdir /s /q Protocol
